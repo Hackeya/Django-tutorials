bde511b7763be5b0e0523aaf7760b385628a25e7
Hackeya <shavkatovjavohir22@gmail.com>
2024-01-20 17:35:49 +0500

    done

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # local app
-    'hello_world.apps.HelloWorldConfig'
+    # 'hello_world.apps.HelloWorldConfig'
+    'hello_world'
+    # 'pages'
 ]
 
 MIDDLEWARE = [
--- hello_world/views.py
+++ hello_world/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # Create your views here.
 
 def homePageView(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    return HttpResponse('<h1 style="color:blue;background-color:black">Hello world</h1> <h2 style="color:red">HACKER</h2> <br> <h2 style="color:green">HACKER</h2>')
