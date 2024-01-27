7e4262bf2023d5d495df2d93806fc5e58010cf8a
Hackeya <shavkatovjavohir22@gmail.com>
2024-01-27 17:55:49 +0500

    done

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.views.generic import TemplateView
 # Create your views here.
 
-def homePageView(request):
-    return HttpResponse('<h1 style="color:blue;background-color:black">Hello world</h1> <h2 style="color:red">HACKER</h2> <br> <h2 style="color:green">HACKER</h2>')
+# def homePageView(request):
+#     return HttpResponse('<h1 style="color:blue;background-color:black">Hello world</h1> <h2 style="color:red">HACKER</h2> <br> <h2 style="color:green">HACKER</h2>')
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
